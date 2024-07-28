d0beb567801daa5fd06417c96b14a113527348da
7erhiy <7erhiy@gmail.com>
2024-07-28 17:08:58 -0300

    change movie-item style

--- src/components/movieItem/index.js
+++ src/components/movieItem/index.js
@@ -6,42 +6,53 @@ export default function MovieItem({ movieItemData }) {
     ? `https://image.tmdb.org/t/p/w440_and_h660_face${movieItemData.backdrop_path}`
     : posterImage;
 
-    let title = movieItemData.original_title;
+  let title = movieItemData.original_title;
 
-    if (title.length > 24) {
-      title = title.substring(0, 19) + "..."
-
-    }
+  if (title.length > 24) {
+    title = title.substring(0, 19) + "...";
+  }
   return (
-    <div
-      style={{
-        width: "100%",
-        padding: "0.5em",
-        // overflow: "hidden",
-        position: "relative",
-        paddingBottom: "26%",
-      }}
-      className="flex flex-col border rounded-xl border-gray-800"
-    >
-      <div
-      className="flex flex-col text-center"
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          width: "100%",
-          height: "100%",
-          gap: "0.5em",
-        }}
-      >
+    <div>
+      <div>
         <img
           src={posterPath}
           alt="Movie Poster"
           className="w-full h-full object-cover rounded-t-lg"
         />
+      </div>
+      <div>
         <p className="text-blue-50 text-base font-light">{title}</p>
       </div>
-      <div>{/* TODO: MOVIE INFO */}</div>
     </div>
+    // <div
+    //   style={{
+    //     width: "100%",
+    //     padding: "0.5em",
+    //     // overflow: "hidden",
+    //     position: "relative",
+    //     paddingBottom: "26%",
+    //   }}
+    //   className="flex flex-col border rounded-xl border-gray-800"
+    // >
+    //   <div
+    //   className="flex flex-col text-center"
+    //     style={{
+    //       position: "absolute",
+    //       top: 0,
+    //       left: 0,
+    //       width: "100%",
+    //       height: "100%",
+    //       gap: "0.5em",
+    //     }}
+    //   >
+    // <img
+    //   src={posterPath}
+    //   alt="Movie Poster"
+    //   className="w-full h-full object-cover rounded-t-lg"
+    // />
+    // <p className="text-blue-50 text-base font-light">{title}</p>
+    //   </div>
+    //   <div>{/* TODO: MOVIE INFO */}</div>
+    // </div>
   );
 }
