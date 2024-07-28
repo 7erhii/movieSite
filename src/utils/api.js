4a24210a4a8445513cd3d4abae43153816598652
7erhiy <7erhiy@gmail.com>
2024-07-28 19:10:41 -0300

    hig

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,3 @@
-// utils/api.js
 import axios from "axios";
 
 const axiosInstance = axios.create({
@@ -6,8 +5,7 @@ const axiosInstance = axios.create({
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3OTgxZDZlMjIyYzlhMDFlMDA5YWY5ZTRkNmE0ZDU4ZCIsIm5iZiI6MTcyMjEyMTc3NC44MDA5NDcsInN1YiI6IjY2YTE5NzhhYmQyMzRmMTdmZDBiZjI4YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EgWNlHCqbE_dKbr4ehPPCN7vKaiCQAw4pPxNLc7LUy8",
+    Authorization: `Bearer ${process.env.REACT_APP_TMDB_API_KEY}`,
   },
 });
 
